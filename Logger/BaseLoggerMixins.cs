747eb33026a2bb3eb60b5a8efd38288c1d2e3eb9
Ulises <ulisesaguilar2019@gmail.com>
2025-10-05 15:24:13 -0700

    Implementing extension methods
    I implemented the extension methods and changed the configuration properties.

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,32 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
+    public static void Error(string message, BaseLogger baselogger, params object[] args)
+    {
+        if (baselogger is null) throw new ArgumentNullException(nameof(baselogger));
+        baselogger.Log(LogLevel.Error, string.Format(message, args));
+    }
 
+    public static void Warning(string message, BaseLogger baselogger, params object[] args)
+    {
+        if (baselogger is null) throw new ArgumentNullException(nameof(baselogger));
+        baselogger.Log(LogLevel.Error, string.Format(message, args));
+    }
+
+    public static void Information(string message, BaseLogger baselogger, params object[] args)
+    {
+        if (baselogger is null) throw new ArgumentNullException(nameof(baselogger));
+        baselogger.Log(LogLevel.Error, string.Format(message, args));
+    }
+
+    public static void Debug(string message, BaseLogger baselogger, params object[] args)
+    {
+        if (baselogger is null) throw new ArgumentNullException(nameof(baselogger));
+        baselogger.Log(LogLevel.Error, string.Format(message, args));
+    } 
+    
+    
 }
